17acead5a49bec63dfff3e4580959a156936d86b
Vlad <dr.melix@gmail.com>
2024-05-24 15:18:14 +0300

    Создал папку для картинок с помощью os

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+import os
 import requests
 
 
 url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
 
 
+os.makedirs('images', exist_ok=True)
+
+
+file_path = os.path.join('images', 'hubble.jpeg')
+
+
 response = requests.get(url)
 
 
-with open('hubble.jpeg', 'wb') as file:
+with open(file_path, 'wb') as file:
     file.write(response.content)
 
-print("Изображение сохранено как hubble.jpeg")
+print(f"Изображение сохранено как {file_path}")
