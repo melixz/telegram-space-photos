e5ae3e62dc1084e1b7a0c0c90a80c8c774c595b3
Vlad <dr.melix@gmail.com>
2024-05-29 17:42:01 +0300

    Скачал много картинок дня, добавил защиту от ошибок в GET параметрах

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import requests
 from dotenv import load_dotenv
-from urllib.parse import urlsplit, unquote
+from urllib.parse import urlsplit, unquote, urlencode
 
 
 def download_image(url, save_path):
@@ -59,33 +59,38 @@ def get_file_extension_from_url(url):
     return extension
 
 
-def download_nasa_apod_image(api_key):
+def download_nasa_apod_images(api_key, count=50):
     apod_url = "https://api.nasa.gov/planetary/apod"
-    params = {'api_key': api_key, 'hd': True}
-    response = requests.get(apod_url, params=params)
-    response.raise_for_status()
-    apod_data = response.json()
-    image_url = apod_data.get('url')
-    if not image_url:
-        print("URL изображения не найден.")
-        return
+    params = {'api_key': api_key, 'count': count}
 
-    file_extension = get_file_extension_from_url(image_url)
-    save_path = os.path.join('images', f'nasa_apod_today{file_extension}')
-    os.makedirs(os.path.dirname(save_path), exist_ok=True)
-    response_img = requests.get(image_url)
-    response_img.raise_for_status()
-    with open(save_path, 'wb') as file:
-        file.write(response_img.content)
-    print(f"Изображение сохранено как {save_path}")
+    # Кодируем параметры
+    encoded_params = urlencode(params)
+    full_url = f"{apod_url}?{encoded_params}"
+
+    response = requests.get(full_url)
+    response.raise_for_status()
+    apod_data_list = response.json()
+
+    os.makedirs('images', exist_ok=True)
+
+    for index, apod_data in enumerate(apod_data_list):
+        image_url = apod_data.get('url')
+        if not image_url:
+            print(f"URL изображения не найден для записи {index}.")
+            continue
+
+        file_extension = get_file_extension_from_url(image_url)
+        save_path = os.path.join('images', f'nasa_apod_{index}{file_extension}')
+        response_img = requests.get(image_url)
+        response_img.raise_for_status()
+        with open(save_path, 'wb') as file:
+            file.write(response_img.content)
+        print(f"Изображение сохранено как {save_path}")
 
 
 load_dotenv()
 api_key = os.environ.get('NASA_API_KEY')
-download_nasa_apod_image(api_key)
-
-
-fetch_spacex_last_launch()
+download_nasa_apod_images(api_key, count=50)
 
 test_url = "https://example.com/txt/hello%20world.txt?v=9#python"
 print(get_file_extension_from_url(test_url))
