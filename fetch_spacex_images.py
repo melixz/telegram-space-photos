2025944b41bce3a9ec30057768aacc98f05c9274
Vlad <dr.melix@gmail.com>
2024-06-03 17:25:36 +0300

    В скрипте fetch_nasa_apod_images скачиваются все фотографии(если фотографий больше одной)

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -12,40 +12,37 @@ def fetch_spacex_image_urls_by_flight_id(api_url, flight_id):
     return image_urls
 
 
-def fetch_latest_spacex_image_url(api_url, save_path, params=None):
-    os.makedirs(os.path.dirname(save_path), exist_ok=True)
-    response = requests.get(api_url, params=params)
+def fetch_latest_spacex_image_urls(api_url):
+    response = requests.get(api_url)
     response.raise_for_status()
     launch_data = response.json()
-    image_url = launch_data['links']['flickr']['original'][0] if launch_data['links']['flickr']['original'] else None
-    if image_url:
-        download_image(image_url, save_path)
-    else:
-        print("Изображение не найдено в данных последнего запуска")
+    image_urls = launch_data['links']['flickr']['original'] if launch_data['links']['flickr']['original'] else []
+    return image_urls
 
 
-def save_spacex_photos_to_folder(api_url, flight_id):
-    image_urls = fetch_spacex_image_urls_by_flight_id(api_url, flight_id)
+def save_spacex_photos_to_folder(image_urls, folder_name='images'):
     if not image_urls:
         print("Фотографии не найдены.")
         return
 
-    os.makedirs('images', exist_ok=True)
+    os.makedirs(folder_name, exist_ok=True)
 
     for index, url in enumerate(image_urls):
-        save_path = f'images/spacex_{index}.jpg'
+        save_path = os.path.join(folder_name, f'spacex_{index}.jpg')
         download_image(url, save_path)
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Загрузить фотографии SpaceX по ID запуска')
+    parser = argparse.ArgumentParser(description='Загрузить фотографии SpaceX по ID запуска или последнего запуска')
     parser.add_argument('--flight_id', type=str, help='ID запуска SpaceX')
     args = parser.parse_args()
 
     if args.flight_id:
-        save_spacex_photos_to_folder('https://api.spacexdata.com/v5/launches', args.flight_id)
+        image_urls = fetch_spacex_image_urls_by_flight_id('https://api.spacexdata.com/v5/launches', args.flight_id)
+        save_spacex_photos_to_folder(image_urls, folder_name=f'images/{args.flight_id}')
     else:
-        fetch_latest_spacex_image_url('https://api.spacexdata.com/v5/launches/latest', 'images/spacex_latest.jpeg')
+        image_urls = fetch_latest_spacex_image_urls('https://api.spacexdata.com/v5/launches/latest')
+        save_spacex_photos_to_folder(image_urls, folder_name='images/latest')
 
 
 if __name__ == '__main__':
