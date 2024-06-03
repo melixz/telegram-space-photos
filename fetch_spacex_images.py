067a24ff336f4c41ee913dd2f373ff7f4bade31f
Vlad <dr.melix@gmail.com>
2024-06-03 17:11:07 +0300

    Уточнил название функций в скрипте fetch_nasa_apod_images

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -4,15 +4,28 @@ import argparse
 from common import download_image
 
 
-def spacex_api_by_id(api_url, flight_id):
+def fetch_spacex_image_urls_by_flight_id(api_url, flight_id):
     response = requests.get(f"{api_url}/{flight_id}")
     response.raise_for_status()
-    rocket_launch_data = response.json()
-    image_urls = rocket_launch_data['links']['flickr']['original'] if rocket_launch_data['links']['flickr']['original'] else []
+    launch_data = response.json()
+    image_urls = launch_data['links']['flickr']['original'] if launch_data['links']['flickr']['original'] else []
     return image_urls
 
 
-def save_spacex_photos_to_folder(image_urls):
+def fetch_latest_spacex_image_url(api_url, save_path, params=None):
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+    response = requests.get(api_url, params=params)
+    response.raise_for_status()
+    launch_data = response.json()
+    image_url = launch_data['links']['flickr']['original'][0] if launch_data['links']['flickr']['original'] else None
+    if image_url:
+        download_image(image_url, save_path)
+    else:
+        print("Изображение не найдено в данных последнего запуска")
+
+
+def save_spacex_photos_to_folder(api_url, flight_id):
+    image_urls = fetch_spacex_image_urls_by_flight_id(api_url, flight_id)
     if not image_urls:
         print("Фотографии не найдены.")
         return
@@ -30,10 +43,9 @@ def main():
     args = parser.parse_args()
 
     if args.flight_id:
-        image_urls = spacex_api_by_id('https://api.spacexdata.com/v5/launches', args.flight_id)
-        save_spacex_photos_to_folder(image_urls)
+        save_spacex_photos_to_folder('https://api.spacexdata.com/v5/launches', args.flight_id)
     else:
-        print("ID запуска не указан")
+        fetch_latest_spacex_image_url('https://api.spacexdata.com/v5/launches/latest', 'images/spacex_latest.jpeg')
 
 
 if __name__ == '__main__':
